84132d5dcdfdb9f9c463f188cab0d75033babc8e
Thiago Lins <taglins@gmail.com>
2021-11-09 13:52:25 -0300

    [Fix/Update] Correções e importações app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const session = require("express-session")
+const express = require("express");
+const session = require("express-session");
 const path = require("path");
+const logger = require("morgan");
 const app = express();
+const methodOverride = require("method-override");
+
 //Definindo utilização de rotas
-const rotas = require("./src/routes");
-const rotasDeProdutos = require("./src/routes/produtos");
-const rotasAdmin = require('./src/routes/admin');
+const mainRoutes = require("./src/routes");
+const productsRoutes = require("./src/routes/produtos");
+const adminRoutes = require("./src/routes/admin");
 const port = 3000;
 
-const logger = require("morgan");
 app.use(logger("dev"));
 /*https://expressjs.com/en/resources/middleware/morgan.html*/
 
@@ -22,23 +24,23 @@ app.use(
 );
 
 //Definindo Pasta Pública
-app.use(express.static(path.join(__dirname, "src/public")));
-
+app.use(express.static(path.resolve(__dirname, "src", "public")));
+//Definindo methodOverride para trabalhar com métodos HTTP
+app.use(methodOverride("_method"));
 //Definindo view Engine e Pasta Views
 app.set("view engine", "ejs");
-app.set("views", path.resolve("./src/views"));
-
-
+app.set("views", path.resolve(__dirname, "src", "views"));
 
 // Definindo uso de JSON
 app.use(express.json()); /*Adicionado do print de aula 27072021*/
 // Desabilitando Encoded
-app.use(express.urlencoded({ extended: false })
+app.use(
+  express.urlencoded({ extended: false })
 ); /*Adicionado do print de aula 27072021*/
-app.use(rotas);
-app.use("/produtos", rotasDeProdutos);
-app.use("/admin", rotasAdmin);
 
+app.use(mainRoutes);
+app.use("/produtos", productsRoutes);
+app.use("/admin", adminRoutes);
 
 app.listen(port, () => {
   console.log(`O servidor esta rodando em http://localhost:${port}`);
