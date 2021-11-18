ab5bafa8e480bdf9e5b5143fb8c7d6b5ab605fba
Thiago Lins <taglins@gmail.com>
2021-11-18 03:34:08 -0300

    [Add] Inicializando Estrutura dos Controllers

--- src/controller/CategoryController.js
+++ src/controller/CategoryController.js
@@ -1,5 +1,41 @@
 const CategoryController = {
-  // Criar rotas de Categoria
+  viewAllPage(req, res) {},
+  async detailPage(req, res) {
+    try {
+      const { id } = req.params;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  createPage(req, res) {
+    // Página criara apenas categoria não tendo consulta ao DB diretamente
+  },
+  async listAll(req, res) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async create(req, res) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async update(req, res) {
+    const { id } = req.params;
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async delete(req, res) {
+    const { id } = req.params;
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = CategoryController;
--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,5 +1,45 @@
 const UserController = {
-  // Criar rotas de Produto
+  async viewAllPage(req, res) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async detailPage(req, res) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  createPage(req, res) {
+    //Apenas exibira á pagina de criação de usuário não sendo necessário por hora consulta direta ao DB
+  },
+  async listAll(req, res) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async create(req, res) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async delete(req, res) {
+    const { id } = req.params;
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = UserController;
